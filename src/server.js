84269f04a58c58ffb50ab270491e0dbae636529c
Maksym Semenov <max_semenov@ukr.net>
2025-09-27 23:47:51 +0300

    add err

--- src/server.js
+++ src/server.js
@@ -50,7 +50,7 @@ res.status(404).json({ message: 'Route not found' });
 app.use((err, req, res, next) => {
 console.error('Error:', err.message);
  res.status(500).json({
- message: 'Simulated server error',
+   error: err.message,
  });
 });
 
