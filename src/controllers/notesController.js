76655b8b61f7288301869beb2a7e2970c2bf44ef
Maksym Semenov <max_semenov@ukr.net>
2025-10-03 01:56:44 +0300

    add structure

---
+++ src/controllers/notesController.js
@@ -0,0 +1,20 @@
+import createHttpError from 'http-errors';
+import { Note } from "../models/note.js";
+
+
+export const getAllNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.status(200).json(notes);
+};
+
+export const getNoteById = async (req, res, next) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    next(createHttpError(404, 'Note not found'));
+    return;
+  }
+
+  res.status(200).json(note);
+};
