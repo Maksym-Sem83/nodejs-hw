43cb1ccacc814eb0ee006204b6716c44190d2a97
Maksym Semenov <max_semenov@ukr.net>
2025-10-16 23:49:14 +0300

    add auth

--- src/models/user.js
+++ src/models/user.js
@@ -30,4 +30,10 @@ userSchema.pre('save', function (next) {
   next();
 });
 
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export const User = model('User', userSchema);
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 // Middleware 404 (після всіх маршрутів)
