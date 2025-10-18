492b4f954588136885989ced77629725cb71e133
Maksym Semenov <max_semenov@ukr.net>
2025-10-18 21:54:42 +0300

    add all task

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import 'dotenv/config';
+import cookieParser from "cookie-parser";
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
@@ -15,6 +16,7 @@ const PORT = process.env.PORT ?? 3030;
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(logger);
 
 app.use(authRoutes);
