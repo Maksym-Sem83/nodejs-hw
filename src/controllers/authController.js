41cc948e63b66d7031bc4b6e474720aa79237f49
Maksym Semenov <max_semenov@ukr.net>
2025-10-23 01:04:53 +0300

    add structure 05-mail-and-img

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -93,3 +93,14 @@ export const refreshUserSession = async (req, res, next) => {
     message: 'Session refreshed',
   });
 };
+
+
+export const requestResetEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+	res.status(200).json({
+		message: 'Password reset email sent successfully'
+	});
+};
