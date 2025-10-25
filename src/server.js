b373b8643d54f596e0c18d3332b4c02b208d4321
Maksym Semenov <max_semenov@ukr.net>
2025-10-25 23:56:27 +0300

    finish 05-mail-and-img

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -21,6 +22,7 @@ app.use(logger);
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 // Middleware 404 (після всіх маршрутів)
 app.use(notFoundHandler);
